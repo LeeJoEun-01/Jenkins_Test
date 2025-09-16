1ff83b1dfb632a544aa89e096d77051df0098e33
LEEJOEUN-01 <joeun1005@naver.com>
2025-09-16 14:43:39 +0900

    Create step04.gradleBuild

---
+++ src/test/java/edu/fisa/Step04GradleBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step04GradleBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
